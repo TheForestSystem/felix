98e935f682da303933f2e6382d9f1bba6b23fbc1
TheForestSystem <63935304+TheForestSystem@users.noreply.github.com>
2024-03-01 10:17:59 -0600

    Update MethodsAssignmentJava.java

--- src/methodsassignmentjava/MethodsAssignmentJava.java
+++ src/methodsassignmentjava/MethodsAssignmentJava.java
@@ -4,37 +4,37 @@
  */
 package methodsassignmentjava;
 
-import java.io.PrintStream;
-import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-/**
- *
- * @author f.wegert
- */
 public class MethodsAssignmentJava {
-    
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        JOptionPane.showMessageDialog(null, "Welcome to formatting Numbers ", "Formatting Numbers", JOptionPane.PLAIN_MESSAGE);        
+        // Call the number method to get user input and process it
         number();
-        
-    }
-    public static void number(){
-     String number = JOptionPane.showInputDialog(null, "Please enter your number: ", "Formatting Numbers", JOptionPane.PLAIN_MESSAGE);
-        double number2 = Double.parseDouble(number);    
-        double minimum = 1000;
-       
-        
-        if (number2 >= minimum) {
-        System.out.printf("%f, ", number2);   
-       }
-        
-        JOptionPane.showMessageDialog(null, "Formatted Number: " + number2, "Formatting Numbers ", JOptionPane.INFORMATION_MESSAGE);
-        
-     } 
     }
 
+    public static void number() {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.println("Enter a number larger than 1,000: ");
+        try {
+            String raw = reader.readLine();
+            double number = Double.parseDouble(raw);
+            double minimum = 1000;
+
+            if (number >= minimum) {
+                // TODO: Format logic
+                System.out.println( String.format("%,.2f", number));
+            } else {
+                System.out.println("The number entered is less than 1,000.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+        }
+    }
+}
